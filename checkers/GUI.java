9e41ae62b121685a2e80afd53bff7448cbd23ee1
Zach <shasbot@gmail.com>
2009-11-10 11:05:13 -0700

    updating to current state of build 1

--- checkers/GUI.java
+++ checkers/GUI.java
@@ -7,16 +7,26 @@ public class GUI implements ActionListener
 {
 	private JFrame window;
 	private GameScreen gamescr;
+    private SetupScreen setupscr;
 
 GUI()
 {
 	window = new JFrame("Checkers");
 	Container cp = window.getContentPane();
-	gamescr = new GameScreen();     //will likely pass size as parameter in future
-	window.setSize(500,250);
+
+    setupscr = new SetupScreen(8);
+
+	window.setSize(1000,500);
 	window.setVisible(true);
 	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	cp.add(gamescr.panel);
+	cp.add(setupscr.panel);
+    while(!(setupscr.bsetup.all_done()))
+    {
+
+    }
+    cp.remove(setupscr.panel);
+    gamescr = new GameScreen(8,setupscr.setup_game);
+    cp.add(gamescr.panel);    //will run when setup is done
 
 }
 public void actionPerformed(ActionEvent event)  //important, associates ui actions with consequences
